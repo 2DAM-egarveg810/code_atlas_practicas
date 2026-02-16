cb9cebb0e4cc8db2ca60b1ee9f6330fab7f6049d
2DAM-egarveg810 <egarveg810@g.educaand.es>
2026-02-16 09:48:48 +0100

    [ui] añadir header
    He añadido header.html para que pueda haber navegación, me falta configurarlo.
    También he hecho una función en el modelo de snippet para que el color de la badge cambie en base al lenguaje.

--- snippets/models.py
+++ snippets/models.py
@@ -26,7 +26,8 @@ class Snippet(models.Model):
     title = models.CharField(max_length=50)
     source_code = models.TextField()
     description = models.TextField(null=True, blank=True)
-    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='snippets', default=1, null=True, blank=True)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='snippets', default=1, null=True,
+                               blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     pub_update = models.DateTimeField(auto_now=True)
     language = models.CharField(
@@ -39,3 +40,18 @@ class Snippet(models.Model):
 
     def __str__(self):
         return f'{self.title} [{self.language}]'
+    
+    def get_language_badge_color(self):
+        return {
+            'python': 'bg-primary',
+            'javascript': 'bg-warning text-dark',
+            'typescript': 'bg-info text-dark',
+            'html': 'bg-danger',
+            'css': 'bg-secondary',
+            'django': 'bg-success',
+            'sql': 'bg-dark',
+            'java': 'bg-danger',
+            'php': 'bg-indigo',
+            'kotlin': 'bg-purple',
+            'markdown': 'bg-light text-dark',
+        }.get(self.language, 'bg-primary')
